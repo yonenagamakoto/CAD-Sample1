ed2e6a2152545180feb9f235d0aea704a9918baf
Makoto Yonenaga <yonenaga.cnet@gmail.com>
2025-04-15 12:54:02 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Sample1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.rdoColumn = new System.Windows.Forms.RadioButton();
+			this.rdoBeam = new System.Windows.Forms.RadioButton();
+			this.rdoHBrace = new System.Windows.Forms.RadioButton();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.btnDelete = new System.Windows.Forms.Button();
+			this.dp = new CNET.DrawingPanel();
+			this.rdoText = new System.Windows.Forms.RadioButton();
+			this.rdoDimension = new System.Windows.Forms.RadioButton();
+			this.btnChange = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dp)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// rdoColumn
+			// 
+			this.rdoColumn.Location = new System.Drawing.Point(10, 30);
+			this.rdoColumn.Name = "rdoColumn";
+			this.rdoColumn.Size = new System.Drawing.Size(80, 20);
+			this.rdoColumn.TabIndex = 1;
+			this.rdoColumn.Text = "柱";
+			this.rdoColumn.UseVisualStyleBackColor = true;
+			// 
+			// rdoBeam
+			// 
+			this.rdoBeam.Checked = true;
+			this.rdoBeam.Location = new System.Drawing.Point(10, 60);
+			this.rdoBeam.Name = "rdoBeam";
+			this.rdoBeam.Size = new System.Drawing.Size(80, 20);
+			this.rdoBeam.TabIndex = 2;
+			this.rdoBeam.TabStop = true;
+			this.rdoBeam.Text = "梁";
+			this.rdoBeam.UseVisualStyleBackColor = true;
+			// 
+			// rdoHBrace
+			// 
+			this.rdoHBrace.Location = new System.Drawing.Point(10, 90);
+			this.rdoHBrace.Name = "rdoHBrace";
+			this.rdoHBrace.Size = new System.Drawing.Size(80, 20);
+			this.rdoHBrace.TabIndex = 3;
+			this.rdoHBrace.Text = "水平ﾌﾞﾚｰｽ";
+			this.rdoHBrace.UseVisualStyleBackColor = true;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Location = new System.Drawing.Point(10, 200);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(75, 23);
+			this.btnAdd.TabIndex = 4;
+			this.btnAdd.Text = "追加";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			// 
+			// btnDelete
+			// 
+			this.btnDelete.Location = new System.Drawing.Point(10, 230);
+			this.btnDelete.Name = "btnDelete";
+			this.btnDelete.Size = new System.Drawing.Size(75, 23);
+			this.btnDelete.TabIndex = 5;
+			this.btnDelete.Text = "削除";
+			this.btnDelete.UseVisualStyleBackColor = true;
+			// 
+			// dp
+			// 
+			this.dp.AddGridLine = true;
+			this.dp.AllowMove = false;
+			this.dp.AllowObjectMove = false;
+			this.dp.AllowResize = false;
+			this.dp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dp.AreaZoom = false;
+			this.dp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.dp.BottomMargin = 50;
+			this.dp.CheckApplicationName = false;
+			this.dp.DrawingMode = CNET.DrawingModeEnum.HighQuality;
+			this.dp.DrawingRatio = 0F;
+			this.dp.DrawingScale = 0F;
+			this.dp.GridLineColor = System.Drawing.Color.White;
+			this.dp.GridLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+			this.dp.GridLineVisible = true;
+			this.dp.GridLineWidth = 1F;
+			this.dp.GridShadowColor = System.Drawing.Color.DarkGray;
+			this.dp.HighSpeedSnap = true;
+			this.dp.ImageData = null;
+			this.dp.ImageDensity = 1F;
+			this.dp.ImageOrigin = ((System.Drawing.PointF)(resources.GetObject("dp.ImageOrigin")));
+			this.dp.ImageSize = new System.Drawing.Size(0, 0);
+			this.dp.IsMoving = false;
+			this.dp.IsResizing = false;
+			this.dp.IsSnapFree = false;
+			this.dp.LastDrawArcLayers = null;
+			this.dp.LastDrawCircleLayers = null;
+			this.dp.LastDrawDimensionLayers = null;
+			this.dp.LastDrawLineLayers = null;
+			this.dp.LastDrawPointLayers = null;
+			this.dp.LastDrawPolygonLayers = null;
+			this.dp.LastDrawPolylineLayers = null;
+			this.dp.LastDrawRectangleLayers = null;
+			this.dp.LastDrawStringLayers = null;
+			this.dp.LastDrawTableLayers = null;
+			this.dp.LeftMargin = 50;
+			this.dp.LicenseKey = "";
+			this.dp.Location = new System.Drawing.Point(95, 5);
+			this.dp.MarkerColor = System.Drawing.Color.Black;
+			this.dp.MarkerFill = true;
+			this.dp.MarkerRect = new System.Drawing.Rectangle[] {
+        new System.Drawing.Rectangle(0, 0, 0, 0),
+        new System.Drawing.Rectangle(0, 0, 0, 0),
+        new System.Drawing.Rectangle(0, 0, 0, 0),
+        new System.Drawing.Rectangle(0, 0, 0, 0),
+        new System.Drawing.Rectangle(0, 0, 0, 0),
+        new System.Drawing.Rectangle(0, 0, 0, 0),
+        new System.Drawing.Rectangle(0, 0, 0, 0),
+        new System.Drawing.Rectangle(0, 0, 0, 0),
+        new System.Drawing.Rectangle(0, 0, 0, 0)};
+			this.dp.MarkerShape = CNET.PointShapeEnum.Square;
+			this.dp.MarkerSize = new System.Drawing.Size(10, 10);
+			this.dp.MarkerStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+			this.dp.MarkerWidth = 1F;
+			this.dp.MaxUndo = 100;
+			this.dp.MaxVertex = 100;
+			this.dp.MinusXGrid = 0;
+			this.dp.MinusYGrid = 0;
+			this.dp.Mode = CNET.ModeEnum.Neutral;
+			this.dp.Name = "dp";
+			this.dp.Object = "";
+			this.dp.OfflineCode = "";
+			this.dp.OffsetX = 0F;
+			this.dp.OffsetY = 0F;
+			this.dp.Ratio = 0.03067766F;
+			this.dp.RedoFileName = "";
+			this.dp.RightAngleOnly = false;
+			this.dp.RightMargin = 50;
+			this.dp.SelectMode = CNET.SelectModeEnum.Single;
+			this.dp.SerialCode = "";
+			this.dp.ShowCenterMarker = false;
+			this.dp.ShowConerMarker = false;
+			this.dp.ShowMidMarker = false;
+			this.dp.Size = new System.Drawing.Size(700, 435);
+			this.dp.SnapCrossPoint = false;
+			this.dp.SnapDimensionEnd = false;
+			this.dp.SnapGuideRectangleVertex = false;
+			this.dp.SnapLineEnd = false;
+			this.dp.SnapMode = CNET.SnapModeEnum.Default;
+			this.dp.SnapPointObject = false;
+			this.dp.SnapPolygonSideCrossPoint = false;
+			this.dp.SnapPolygonVertex = false;
+			this.dp.SnapRectangleVertex = false;
+			this.dp.StretchPointSize = new System.Drawing.SizeF(50F, 50F);
+			this.dp.TabIndex = 0;
+			this.dp.TableTemp = null;
+			this.dp.TabStop = false;
+			this.dp.TextRatio = 0.75F;
+			this.dp.TopMargin = 50;
+			this.dp.TrimR = 500F;
+			this.dp.UndoFileName = "";
+			this.dp.UseRightClick = false;
+			this.dp.X = 0F;
+			this.dp.XGrid = 24;
+			this.dp.XMax = 10920F;
+			this.dp.XPitch = 455F;
+			this.dp.Y = 0F;
+			this.dp.YGrid = 24;
+			this.dp.YMax = 10920F;
+			this.dp.YPitch = 455F;
+			this.dp.ZoomingRatio = 1F;
+			// 
+			// rdoText
+			// 
+			this.rdoText.Location = new System.Drawing.Point(10, 150);
+			this.rdoText.Name = "rdoText";
+			this.rdoText.Size = new System.Drawing.Size(80, 20);
+			this.rdoText.TabIndex = 6;
+			this.rdoText.Text = "ﾃｷｽﾄ";
+			this.rdoText.UseVisualStyleBackColor = true;
+			// 
+			// rdoDimension
+			// 
+			this.rdoDimension.Location = new System.Drawing.Point(10, 120);
+			this.rdoDimension.Name = "rdoDimension";
+			this.rdoDimension.Size = new System.Drawing.Size(80, 20);
+			this.rdoDimension.TabIndex = 7;
+			this.rdoDimension.Text = "寸法線";
+			this.rdoDimension.UseVisualStyleBackColor = true;
+			// 
+			// btnChange
+			// 
+			this.btnChange.Location = new System.Drawing.Point(10, 260);
+			this.btnChange.Name = "btnChange";
+			this.btnChange.Size = new System.Drawing.Size(75, 23);
+			this.btnChange.TabIndex = 8;
+			this.btnChange.Text = "変更";
+			this.btnChange.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnChange);
+			this.Controls.Add(this.rdoDimension);
+			this.Controls.Add(this.rdoText);
+			this.Controls.Add(this.btnDelete);
+			this.Controls.Add(this.btnAdd);
+			this.Controls.Add(this.rdoHBrace);
+			this.Controls.Add(this.rdoBeam);
+			this.Controls.Add(this.rdoColumn);
+			this.Controls.Add(this.dp);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dp)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private CNET.DrawingPanel dp;
+		private System.Windows.Forms.RadioButton rdoColumn;
+		private System.Windows.Forms.RadioButton rdoBeam;
+		private System.Windows.Forms.RadioButton rdoHBrace;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Button btnDelete;
+		private System.Windows.Forms.RadioButton rdoText;
+		private System.Windows.Forms.RadioButton rdoDimension;
+		private System.Windows.Forms.Button btnChange;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using CNET;
+
+namespace Sample1
+{
+	public partial class Form1 : Form
+	{
+		private string _Obj = "";//選択オブジェクト
+		private string _Mode = "";//モード
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			//PointObjectの初期設定
+			dp.Points.Shape = PointShapeEnum.H;
+			dp.Points.Size = new SizeF(400, 400);
+			dp.Points.Width = 2;
+			dp.Points.Color = Color.Red;
+			//LineObjectの初期設定
+			dp.Lines.Thickness = 250;
+			dp.Lines.Color = Color.Blue;
+			dp.Lines.Width = 1;
+			dp.Lines.ExtLeftLength = -200;
+			dp.Lines.ExtRightLength = -200;
+			//RadioButtonのCheckedChangedイベント
+			rdoColumn.CheckedChanged += Rdo_CheckedChanged;
+			rdoBeam.CheckedChanged += Rdo_CheckedChanged;
+			rdoHBrace.CheckedChanged += Rdo_CheckedChanged;
+			rdoDimension.CheckedChanged += Rdo_CheckedChanged;
+			rdoText.CheckedChanged += Rdo_CheckedChanged;
+			//ButtonのClickイベント
+			btnAdd.Click += BtnAdd_Click;
+			btnDelete.Click += BtnDelete_Click;
+			btnChange.Click += BtnChange_Click;
+			//DrawingPanelのDrawnイベント
+			dp.RectangleDrawn += Dp_RectangleDrawn;
+			dp.DimensionDrawn += Dp_DimensionDrawn;
+			//DrawingPanelのSelectedイベント
+			dp.PointSelected += Dp_PointSelected;
+			dp.LineSelected += Dp_LineSelected;
+			dp.DimensionSelected += Dp_DimensionSelected;
+			dp.StringSelected += Dp_StringSelected;
+			//DrawingPanelの入力キャンセル処理イベント
+			dp.MouseUp += Dp_MouseUp;
+		}
+
+		private void Rdo_CheckedChanged(object sender, EventArgs e)
+		{
+			RadioButton rdo = (RadioButton)sender;
+			if (!rdo.Checked) return;
+			//オブジェクトをセット
+			if (rdo == rdoColumn)
+				_Obj = "Column";
+			else if (rdo == rdoBeam)
+				_Obj = "Beam";
+			else if (rdo == rdoHBrace)
+				_Obj = "HBrace";
+			else if (rdo == rdoDimension)
+				_Obj = "Dimension";
+			else if (rdo == rdoText)
+				_Obj = "Text";
+			//モードに応じてイベントハンドラーへ
+			if (_Mode == "Add")
+				this.BtnAdd_Click(rdo, EventArgs.Empty);
+			else if (_Mode == "Delete")
+				this.BtnDelete_Click(rdo, EventArgs.Empty);
+			else if (_Mode == "Change")
+				this.BtnChange_Click(rdo, EventArgs.Empty);
+		}
+
+		private void BtnAdd_Click(object sender, EventArgs e)
+		{
+			_Mode = "Add";
+			//DrawingPanelのモードを設定
+			if (_Obj == "Column")
+				dp.Mode = ModeEnum.DrawPoint;
+			else if (_Obj == "Beam")
+				dp.Mode = ModeEnum.DrawLine;
+			else if (_Obj == "HBrace")
+				dp.Mode = ModeEnum.DrawRectangle;
+			else if (_Obj == "Dimension")
+				dp.Mode = ModeEnum.DrawDimension;
+			else if (_Obj == "Text")
+			{
+				dp.Mode = ModeEnum.DrawString;
+				//テキスト入力
+				string text = Interaction.InputBox("テキストを入力して下さい。", "テキスト");
+				if (text != "")
+				{
+					//dp.StringBufferにStringObjectを追加
+					dp.StringBuffer.Add(text, 0, new Font("ＭＳ ゴシック", 9));
+				}
+			}
+		}
+
+		private void BtnDelete_Click(object sender, EventArgs e)
+		{
+			_Mode = "Delete";
+			//DrawingPanelのモードを設定
+			if (_Obj == "Column")
+				dp.Mode = ModeEnum.RemovePoint;
+			else if (_Obj == "Beam")
+				dp.Mode = ModeEnum.RemoveLine;
+			else if (_Obj == "HBrace")
+				dp.Mode = ModeEnum.RemoveRectangle;
+			else if (_Obj == "Dimension")
+				dp.Mode = ModeEnum.RemoveDimension;
+			else if (_Obj == "Text")
+				dp.Mode = ModeEnum.RemoveString;
+		}
+
+		private void BtnChange_Click(object sender, EventArgs e)
+		{
+			_Mode = "Change";
+			//DrawingPanelのモードを設定
+			if (_Obj == "Column")
+				dp.Mode = ModeEnum.SelectPoint;
+			else if (_Obj == "Beam")
+				dp.Mode = ModeEnum.SelectLine;
+			else if (_Obj == "HBrace")
+				dp.Mode = ModeEnum.SelectLine;
+			else if (_Obj == "Dimension")
+				dp.Mode = ModeEnum.SelectDimension;
+			else if (_Obj == "Text")
+				dp.Mode = ModeEnum.SelectString;
+		}
+
+		private void Dp_MouseUp(object sender, MouseEventArgs e)
+		{
+			//右ボタンクリックで入力キャンセル
+			if (e.Button == MouseButtons.Right)
+			{
+				//PointBufferをクリアしてラバーバンドを消去する
+				dp.ClearPointBuffer();
+				dp.RectRubberBand.Erase();
+			}
+		}
+
+		private void Dp_RectangleDrawn(object sender, ObjectDrawnEventArgs e)
+		{
+			if (_Obj != "HBrace") return;
+			//Rectanglesコレクションの中からカレントオブジェクトを取得する
+			RectangleObject rect = dp.Rectangles[e.Index];
+			//rectの右上りの対角線を取得し、Linesコレクションに追加する
+			LineObject hb1 = rect.DiagonalUpward;
+			hb1.Color = Color.DarkGray;
+			hb1.Style = System.Drawing.Drawing2D.DashStyle.Dot;
+			hb1.Width = 2;
+			dp.Lines.Add(hb1);
+			//rectの右下りの対角線を取得し、Linesコレクションに追加する
+			LineObject hb2 = rect.DiagonalDownward;
+			hb2.Color = Color.DarkGray;
+			hb2.Style = System.Drawing.Drawing2D.DashStyle.Dot;
+			hb2.Width = 2;
+			dp.Lines.Add(hb2);
+			//rectを削除する
+			dp.Rectangles.Remove(rect);
+		}
+
+		private void Dp_DimensionDrawn(object sender, ObjectDrawnEventArgs e)
+		{
+			if (_Obj == "Dimension")
+			{
+				//Dimensionsコレクションの中からカレントオブジェクトを取得する
+				DimensionObject dim = dp.Dimensions[e.Index];
+				//寸法値を編集させる
+				string value = Interaction.InputBox("寸法値を入力して下さい。", "寸法線", dim.Text);
+				if (value != "")
+				{
+					dim.Text = value;
+				}
+			}
+		}
+
+		private void Dp_PointSelected(object sender, ObjectSelectedEventArgs e)
+		{
+			if (_Obj == "Column")
+			{
+				//Pointsコレクションの中からカレントオブジェクトを取得する
+				PointObject column = dp.Points[e.Index];
+				if (dp.Mode == ModeEnum.SelectPoint)
+				{
+					//モードがSelectPointであれば、柱形状を変更する
+					if (column.Shape == PointShapeEnum.H)
+						column.Shape = PointShapeEnum.Square;
+					else
+						column.Shape = PointShapeEnum.H;
+				}
+				else if (dp.Mode == ModeEnum.RemovePoint)
+				{
+					//モードがRemovePointであれば、柱を削除する
+					dp.Points.Remove(column);
+				}
+			}
+			//SelectedPointsコレクション内のオブジェクトを削除する
+			dp.ClearSelectedObject();
+		}
+
+		private void Dp_LineSelected(object sender, ObjectSelectedEventArgs e)
+		{
+			if (_Obj == "Beam")
+			{
+				//Linesコレクションの中からカレントオブジェクトを取得する
+				LineObject beam = dp.Lines[e.Index];
+				if (dp.Mode == ModeEnum.SelectLine)
+				{
+					//モードがSelectLineであれば、梁幅と塗り潰し色を変更する
+					if (beam.Thickness == 250)
+					{
+						beam.Thickness = 350;
+						beam.IsFill = true;
+						beam.FillColor = Color.SkyBlue;
+					}
+					else
+					{
+						beam.Thickness = 250;
+						beam.IsFill = false;
+					}
+				}
+				else
+				{
+					//モードがRemoveLineであれば、梁を削除する
+					dp.Lines.Remove(beam);
+				}
+			}
+			else if (_Obj == "HBrace")
+			{
+				//Linesコレクションの中からカレントオブジェクトを取得する
+				LineObject hBrace = dp.Lines[e.Index];
+				if (dp.Mode == ModeEnum.SelectLine)
+				{
+					//モードがSelectLineであれば、ブレース色を変更する
+					if (hBrace.Color == Color.DarkGray)
+						hBrace.Color = Color.Black;
+					else
+						hBrace.Color = Color.DarkGray;
+				}
+				else
+				{
+					//モードがRemoveLineであれば、ブレースを削除する
+					dp.Lines.Remove(hBrace);
+				}
+			}
+			//SelectedPointsコレクション内のオブジェクトを削除する
+			dp.ClearSelectedObject();
+		}
+
+		private void Dp_DimensionSelected(object sender, ObjectSelectedEventArgs e)
+		{
+			if (_Obj == "Dimension")
+			{
+				//Dimensionsコレクションの中からカレントオブジェクトを取得する
+				DimensionObject dim = dp.Dimensions[e.Index];
+				if (dp.Mode == ModeEnum.SelectDimension)
+				{
+					//モードがSelectDimensionであれば、寸法値を変更する
+					string value = Interaction.InputBox("寸法値を入力して下さい。", "寸法線", dim.Text);
+					if (value != "")
+					{
+						dim.Text = value;
+					}
+				}
+				else if (dp.Mode == ModeEnum.RemoveDimension)
+				{
+					//モードがRemoveDimensionであれば、寸法線を削除する
+					dp.Dimensions.Remove(dim);
+				}
+			}
+			//SelectedPointsコレクション内のオブジェクトを削除する
+			dp.ClearSelectedObject();
+		}
+
+		private void Dp_StringSelected(object sender, ObjectSelectedEventArgs e)
+		{
+			if (_Obj == "Text")
+			{
+				//Stringsコレクションの中からカレントオブジェクトを取得する
+				StringObject s = dp.Strings[e.Index];
+				if (dp.Mode == ModeEnum.SelectString)
+				{
+					//モードがSelectStringであれば、テキストを変更する
+					string text = Interaction.InputBox("テキストを入力して下さい。", "テキスト", s.Text);
+					if (text != "")
+					{
+						s.Text = text;
+					}
+				}
+				else if (dp.Mode == ModeEnum.RemoveString)
+				{
+					//モードがRemoveStringであれば、テキストを削除する
+					dp.Strings.Remove(s);
+				}
+			}
+			//SelectedPointsコレクション内のオブジェクトを削除する
+			dp.ClearSelectedObject();
+		}
+	}
+}
